ceef049318407be490ce444aaaa9e830d037b54d
prakhar-s <prakhardevsingh4@gmail.com>
2024-01-24 09:35:39 +0000

    refactor: :recycle: sonar suggestion to avoid null error

--- src/main/java/com/strandls/userGroup/service/impl/UserGroupDatatableServiceImpl.java
+++ src/main/java/com/strandls/userGroup/service/impl/UserGroupDatatableServiceImpl.java
@@ -128,7 +128,8 @@ public class UserGroupDatatableServiceImpl implements UserGroupDatatableService 
 				UserGroupIbp ugIbp = userGroupService.fetchByGroupIdIbp(ug.getUserGroupId());
 				String description = userGroupService.createUgDescription(ugIbp);
 
-				MailData mailData = generateMailData(request, datatableId, userGroupDataTableData);
+				MailData mailData = new MailData();
+				mailData = generateMailData(request, datatableId, userGroupDataTableData);
 				DataTableMailData datatableMailData = mailData.getDataTableMailData();
 				datatableMailData.setAuthorId(Long.parseLong(userGroupDataTableData.getContributor()));
 
